422bf19d6400521de85129a4cbbc3826cbc767f7
AbhaySemwal <shbhtsemwal@gmail.com>
2024-09-23 00:00:42 +0530

    rating

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
       <ul className="nav-links">
         <li><Link href="/">Home</Link></li>
         <li><Link href="/about">About</Link></li>
+        <li><Link href="/Rate">Rate</Link></li>
         <li><Link href="/services">Services</Link></li>
         <li><Link href="/contact">Contact</Link></li>
       </ul>
